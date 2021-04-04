cf0a5bd6df4e394541f575507fe763b152aebad2
Sophia Wang <sophiawa@andrew.cmu.edu>
2021-04-03 21:45:00 -0400

    Add outlines

--- scripts/visHdf5Files.py
+++ scripts/visHdf5Files.py
@@ -2,11 +2,14 @@ import os
 import h5py
 import argparse
 import numpy as np
+import matplotlib.image as mpimg
 from matplotlib import pyplot as plt
 import sys
 import json
 from PIL import Image, ImageDraw
 from scipy import ndimage
+from skimage import morphology
+import cv2
 
 parser = argparse.ArgumentParser("Script to visualize hdf5 files")
 
@@ -69,7 +72,7 @@ def vis_data(key, data, full_hdf5_data, file_label):
             # Visualize channel
             plt.figure()
             plt.title("{} / {} in {}".format(key, channel_label, file_label))
-            plt.imshow(data[:, :, i], cmap='jet')
+            plt.imshow(data[:, :, i])
 
     elif key in args.other_non_rgb_keys:
         # Make sure the data has only one channel, otherwise matplotlib will treat it as an rgb image
@@ -79,10 +82,10 @@ def vis_data(key, data, full_hdf5_data, file_label):
             data = data[:, :, 0]
         
         plt.imshow(data, cmap='jet')
-        if key == "depth":
-            plt.subplot(2,2,3)
-            plt.title('depth')
-            plt.imshow(data, cmap='jet')
+        #if key == "depth":
+        #    plt.subplot(2,2,3)
+        #    plt.title('depth')
+        #    plt.imshow(data, cmap='jet')
 
     elif key in args.rgb_keys:
         plt.imshow(data)
@@ -120,30 +123,133 @@ def vis_file(path):
                         sy = ndimage.sobel(value,axis=1,mode='constant')
                         # Get square root of sum of squares
                         sobel=np.hypot(sx,sy)
-                        print(np.unique(sobel, return_counts=True))
-                        plt.subplot(2,2,1)
-                        plt.title('sobel')
-                        plt.imshow(sobel)
-                    
+                        #print(np.unique(sobel, return_counts=True))
+                        #print(sobel)
 
                         #sobel = np.where(sobel < 1, 0, sobel)
                         #sobel[(1 <= sobel) & (sobel < 10)] = 0.00784314
                         #sobel[(1 <= sobel) & (sobel < 5)] = 0
-                        sobel[5 <= sobel] = 0.00392157
-                        sobel[0.00392157 != sobel] = 0
-                
+                        #sobel[5 <= sobel] = 255
+                        sobel[:,0] = 0
+                        sobel[:,-1] = 0
+                        sobel[0,:] = 0
+                        sobel[-1,:] = 0
+                        #sobel[sobel < 3] = 0
+                        #sobel[sobel >= 5] = 50
+
+                        count = len(sobel) * len(sobel[0])
+                        i = 3
+                        where = np.where(sobel >= i)
+                        while len(where[0]) > 0.1 * count:
+                            i -= 0.5
+                            where = np.where(sobel >= i)
+                        print(i)
+                        #print(bools)
+
+            
+                    
+                    
+                        ok = sobel.astype(int)
+                        wheree = np.where(ok != 0)
+                        #ok[wheree] = 1
+                        ok = ok.astype('uint8')
+                        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+                        kernel = kernel.astype('uint8')
+                        sobel = morphology.dilation(sobel)
+                        wheree = np.where(sobel >= 3)
+                        sobel[wheree] = 10
+
+                        #plt.imshow(sobel)
+                        #print(np.unique(sobel, return_counts=True))
+                        #print(np.unique(hm, return_counts=True))
+                        #print(np.unique(ok, return_counts=True))
 
                         #sobel_im = Image.fromarray(sobel)
+                        #plt.imshow(sobel)
+                        #print(np.unique(sobel, return_counts=True))
                         #sobel_im.show()
                         #draw = ImageDraw.Draw(sobel_im)
                         #draw.bitmap((0, 0), negs, fill=(0))
 
+                        img = cv2.imread('mask.png')
+                        mask = np.zeros(img.shape, dtype=np.uint8)
+                        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                        cnts = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+                        cv2.drawContours(mask, cnts, -1, 5, thickness=3)
+                        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+                        #plt.imshow(mask)
+
+                        where1 = np.where(mask == 0)
+                        where2 = np.where(mask != 0)
+                        sobel = sobel.astype('uint8')
+                        test = cv2.bitwise_and(sobel, gray)
+                        #plt.imshow(test)
+                        maskim = Image.fromarray(mask, mode='L')
+                        #sobel = Image.fromarray(sobel, mode='L')
+                        draw = ImageDraw.Draw(maskim)
+                        #plt.imshow(maskim)
+                        #draw.bitmap((0, 0), sobel, fill=(255))
+                        mask1 = np.array(maskim)
+                        mask1[where] = 0
+                        mask1[:,-3:] = 0
+                        mask1[:,:3] = 0
+                        #mask1[where1] = 0
+                        #where2 = np.where(sobel > 10)
+                        sobel[where2] = 10
+                        test[where2] = 10
+                        #plt.imshow(sobel)
+                        mask2 = mask1 * 255
+                        mask2 = cv2.cvtColor(mask2, cv2.COLOR_GRAY2RGB)
+                        #mask2[where2] = (173, 255, 47)
+                        mask2[where] = (72, 209, 204)
+                        mask2[where2] = (173, 255, 47)
+                        #Image.fromarray(mask2).show()
+                        mask2 = cv2.cvtColor(mask2, cv2.COLOR_BGR2GRAY)
+                        mask1 = morphology.binary_erosion(mask1)
+                        mask1 = mask1.astype(int)
+                        sobel = sobel.astype(int)
+                        test = test.astype(int)
+                        where4 = np.where(sobel != 0)
+                        sobel[where4] = 10
+                        try1 = cv2.add(test, mask1)
+                        where3 = np.where(mask1 != 0)
+                        try1[where3] = 30
+                        print("try1")
+                        print(np.unique(try1))
+                        print("sobel")
+                        print(np.unique(sobel))
+                        print("mask1")
+                        print(np.unique(mask1))
+                        try1[:,-3:] = 0
+                        try1[:,:3] = 0
+                        print(np.unique(try1))
+                        bru1 = np.where(try1 == 1)
+                        bru2 = np.where(try1 == 2)
+                        bru10 = np.where(try1 == 10)
+                        bru30 = np.where(try1 == 30)
+                        try1 = try1.astype('float64')
+                        try1[bru1] = 0.00392157
+                        try1[bru2] = 0.00392157
+                        try1[bru10] = 0.00392157
+                        try1[bru30] = 0.00784314
+                        print(np.unique(try1, return_counts=True))
+                        #plt.imshow(try1)
+                        plt.show()
+                        plt.savefig('outline2.png')
+                        
+                        
+
+                        #mask_im = Image.fromarray(mask)
+                        #draw = ImageDraw.Draw(mask_im)
+                        #draw.bitmap((0, 0), sobel_im, fill=(0))
+                        
+
                         # see edges
                         #print(np.count_nonzero(sobel == 0.00392157))
-                        print(np.unique(sobel, return_counts=True))
-                        plt.subplot(2,2,2)
+                        #print(np.unique(sobel, return_counts=True))
                         plt.title('outlines')
-                        plt.imshow(sobel)
+                        #plt.imshow(mask)
                     
 
 
@@ -152,7 +258,24 @@ def vis_file(path):
     else:
         print("The file does not exist: {}".format(args.hdf5))
 
+'''
+img = cv2.imread('mask.png')
+mask = np.zeros(img.shape, dtype=np.uint8)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cnts = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+cv2.drawContours(mask, cnts, -1, (255,255,255), thickness=3)
+cv2.imshow('img', mask)
+cv2.waitKey(1)
+cv2.destroyAllWindows()
+for i in range(1,5):
+    cv2.waitKey(1)
+
+
+'''
 # Visualize all given files
 for path in args.hdf5_paths:
     vis_file(path)
-plt.show()
+#plt.savefig('examples/coco_annotations/output/trying_outlines.png')
+#plt.show()
